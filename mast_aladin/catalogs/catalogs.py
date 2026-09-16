bcbc19450aa3facbfc091113b6043932abe7484c
Brett M. Morris <bmmorris@stsci.edu>
2026-09-16 16:03:31 -0400

    priority column implementation, demo notebook

--- mast_aladin/catalogs/catalogs.py
+++ mast_aladin/catalogs/catalogs.py
@@ -6,16 +6,16 @@ import astropy.units as u
 from astropy.coordinates import SkyCoord
 from astropy.table import MaskedColumn
 
-
 __all__ = [
     "ConvexHull",
     "RandomSubset",
-    "is_likely_an_observation",
+    "PriorityColumnSubset",
+    "is_likely_an_observation_table",
     "PerformanceCatalog"
 ]
 
 
-def is_likely_an_observation(table):
+def is_likely_an_observation_table(table):
     """
     Returns True if ``table`` does not contain an 's_region' column.
 
@@ -44,7 +44,6 @@ class PerformanceCatalog:
             **catalog_options
     ):
         self.table = table
-        # self.mast_aladin = mast_aladin
         self.catalog_options = dict(**catalog_options)
         self.n_sources_max = n_sources_max
         self.n_sources_drawn = 0
@@ -116,13 +115,13 @@ class PerformanceCatalog:
         self.n_sources_drawn = np.count_nonzero(sources_in_viewport)
 
         # prevent redrawing an existing catalog:
-        if self.name_with_source_count(sources_in_viewport) in self.mast_aladin.overlays:
-            return
+        # if self.name_with_source_count(sources_in_viewport) in self.mast_aladin.overlays:
+        #     return
 
         self.overlay_info = self.mast_aladin.add_table(
             self.table[sources_in_viewport],
             name=self.name_with_source_count(sources_in_viewport),
-            performance_cls=None,  # prevents performance layer checks
+            performance_catalog=False,  # prevents performance layer checks
             **self.catalog_options
         )
 
@@ -145,7 +144,7 @@ class PerformanceCatalog:
         if n_sources < self.n_sources_max:
             # if the viewport has changed and few enough sources will be visible,
             self.show_catalog_without_optimization(sources_in_viewport)
-        elif n_sources >= self.n_sources_max:
+        else:
             # if the viewport contains too many sources
             self.show_catalog_with_optimization(sources_in_viewport)
 
@@ -157,6 +156,9 @@ class PerformanceCatalog:
         if self.n_sources_drawn == n_sources_total:
             return self.name
         return f"{self.name} [{self.n_sources_drawn}/{n_sources_total}]"
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}: {self.name} (n={len(self.table)})>"
 
 
 class ConvexHull(PerformanceCatalog):
@@ -176,8 +178,7 @@ class ConvexHull(PerformanceCatalog):
 
     def show_catalog_with_optimization(self, *args):
         # add the convex hull *only* if the STC-S overlay isn't already displayed:
-        # if self.overlay_info.get('type', '') != 'overlay_stcs':
-        if True:
+        if self.overlay_info.get('type', '') != 'overlay_stcs':
 
             # remove overlay if one is present:
             self.remove_overlay()
@@ -211,7 +212,8 @@ class ConvexHull(PerformanceCatalog):
 class RandomSubset(PerformanceCatalog):
     """
     Visualize a source catalog as scatter marks up to some number of points N.
-    For >N sources, only display a randomly selected subset of sources within the viewport.
+    For >N sources, only display a randomly selected subset of N sources 
+    within the viewport.
     """
 
     def __post_init__(self):
@@ -224,9 +226,6 @@ class RandomSubset(PerformanceCatalog):
         # convert boolean mask to an array index (integer) mask:
         indices_in_viewport = np.flatnonzero(sources_in_viewport)
 
-        # Note: `choice` draws from a uniform probability distribution.
-        # Non-uniform weights can be given to each entry with the kwarg `p`.
-        # This may be useful for mimicking the behavior of HiPSCat
         random_sources_in_viewport = self.rng.choice(
             indices_in_viewport,
             size=self.n_sources_max,
@@ -238,6 +237,79 @@ class RandomSubset(PerformanceCatalog):
         self.overlay_info = self.mast_aladin.add_table(
             self.table[random_sources_in_viewport],
             name=self.name_with_source_count(sources_in_viewport),
-            performance_cls=None,
+            performance_catalog=False,
+            **self.catalog_options
+        )
+
+
+class PriorityColumnSubset(PerformanceCatalog):
+    """
+    Visualize a source catalog as scatter marks up to some number of points N.
+    For >N sources, only display N marks based on their values in
+    the ``table`` column ``priority_column``.
+
+    If ``small_value_high_priority``, the smallest values in ``priority_column`` are
+    given the highest priority. For example, you might use ``small_value_high_priority=True``
+    to prioritize the brightest sources if ``priority_column`` is a magnitude.
+    """
+
+    def __init__(
+            self,
+            table,
+            name=None,
+            ra_column='RAJ2000',
+            dec_column='DEJ2000',
+            n_sources_max=5_000,
+            priority_column_name=None,
+            small_value_high_priority=True,
+            **catalog_options
+    ):
+        self.priority_column_name = priority_column_name
+        self.small_value_high_priority = small_value_high_priority
+
+        super().__init__(
+            table,
+            name=name,
+            ra_column=ra_column,
+            dec_column=dec_column,
+            n_sources_max=n_sources_max,
+            **catalog_options
+        )
+
+    def __post_init__(self):
+        pass
+
+    def show_catalog_with_optimization(self, sources_in_viewport):
+        # remove catalog overlay if one is present:
+        self.remove_overlay()
+
+        # convert boolean mask to an array index (integer) mask:
+        indices_in_viewport = np.flatnonzero(sources_in_viewport)
+        priorities_in_viewport = np.asarray(
+            self.table[self.priority_column_name][indices_in_viewport]
+        )
+
+        # `np.argpartition` finds the smallest `kth` values,
+        # and returns the array with the smallest `kth` values placed
+        # into the first `kth` entries of the output, without sorting the
+        # rest of the array. This is more efficient than a full sort.
+        if self.small_value_high_priority:
+            top_priority_indices = np.argpartition(
+                priorities_in_viewport, kth=self.n_sources_max
+            )[:self.n_sources_max]
+        else:
+            # change the sign and slice for setting the highest value
+            # with the highest priority:
+            top_priority_indices = np.argpartition(
+                priorities_in_viewport, kth=-self.n_sources_max
+            )[-self.n_sources_max:]
+
+        # update before calling `add_table` so `add_source_count_to_name` works
+        self.n_sources_drawn = top_priority_indices.size
+
+        self.overlay_info = self.mast_aladin.add_table(
+            self.table[indices_in_viewport][top_priority_indices],
+            name=self.name_with_source_count(sources_in_viewport),
+            performance_catalog=False,
             **self.catalog_options
         )
